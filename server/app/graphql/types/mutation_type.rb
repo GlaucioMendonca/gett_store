4831e88fe3ae9353ff628a0dd2b3e7ad052bfad9
glaucio.mendonca <glaucio43@hotmail.com>
2021-09-25 17:34:52 -0300

    feat(server): add mutation delete_product

--- server/app/graphql/types/mutation_type.rb
+++ server/app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_product, mutation: Mutations::CreateProduct
     field :update_product, mutation: Mutations::UpdateProduct
+    field :delete_product, mutation: Mutations::DeleteProduct
   end
 end
