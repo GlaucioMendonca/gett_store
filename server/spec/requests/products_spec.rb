8b98b6fba3f7c2d03b2074934878f71db8f67883
glaucio.mendonca <glaucio43@hotmail.com>
2021-09-25 10:51:46 -0300

    feat(server): creating test scenarios for products

---
+++ server/spec/requests/products_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Products', type: :request do
+  describe 'create product' do
+    it 'Create a new valid product' do
+    end
+
+    it 'Create invalid product' do
+    end
+
+    it 'Create product with code already registered' do
+    end
+  end
+
+  describe 'update product' do
+    it 'update product with valid parameters' do
+    end
+
+    it 'update product with invalid parameters' do
+    end
+  end
+
+  describe 'delete product' do
+    it 'delete product by id' do
+    end
+  end
+end
