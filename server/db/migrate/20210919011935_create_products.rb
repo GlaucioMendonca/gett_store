5eb0fa69f9d6b641edf41bc361cdf179ac23086b
glaucio.mendonca <glaucio43@hotmail.com>
2021-09-25 10:27:35 -0300

    feat(server): add code field to products migration

--- server/db/migrate/20210919011935_create_products.rb
+++ server/db/migrate/20210919011935_create_products.rb
@@ -3,6 +3,7 @@ class CreateProducts < ActiveRecord::Migration[6.1]
     create_table :products do |t|
       t.string :name
       t.decimal :price, precision: 10, scale: 2
+      t.string :code, null: false, index: { unique: true }
 
       t.timestamps
     end
