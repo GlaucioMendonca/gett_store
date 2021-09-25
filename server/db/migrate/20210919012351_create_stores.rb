344a65f5c83f835dcadfda14bbe6a80337706796
glaucio.mendonca <glaucio43@hotmail.com>
2021-09-25 10:26:42 -0300

    feat(server): update cnpj field in stores migration

--- server/db/migrate/20210919012351_create_stores.rb
+++ server/db/migrate/20210919012351_create_stores.rb
@@ -3,7 +3,7 @@ class CreateStores < ActiveRecord::Migration[6.1]
     create_table :stores do |t|
       t.string :name
       t.string :address
-      t.integer :cnpj
+      t.string :cnpj, null: false, index: { unique: true }
 
       t.timestamps
     end
