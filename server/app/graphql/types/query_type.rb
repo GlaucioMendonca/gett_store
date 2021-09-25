ba7ff4de140ccee718f92167d53d58c62ecfe340
glaucio.mendonca <glaucio43@hotmail.com>
2021-09-25 17:31:11 -0300

    feat(server): create queries for product

--- server/app/graphql/types/query_type.rb
+++ server/app/graphql/types/query_type.rb
@@ -3,5 +3,18 @@ module Types
     # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
+
+    field :products, [Types::ProductType], null: false
+    field :product, Types::ProductType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def products
+      Product.all
+    end
+
+    def product(id:)
+      Product.find(id)
+    end
   end
 end
