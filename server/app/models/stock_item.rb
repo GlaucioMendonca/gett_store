9944dc0e4b52f8a873dae0666ed8d19f437050c9
glaucio.mendonca <glaucio43@hotmail.com>
2021-09-25 23:55:17 -0300

    refactor(server): add validations to stock_item

--- server/app/models/stock_item.rb
+++ server/app/models/stock_item.rb
@@ -1,2 +1,19 @@
 class StockItem < ApplicationRecord
+  belongs_to :product
+  belongs_to :store
+
+  validates :product, presence: true
+  validates :store, presence: true
+  validate :product_id, :store_id
+  validate :unique_product_and_store, :valid_amount?
+
+  def valid_amount?
+    errors.add(:invalid_amount, 'quantity must be greater than or equal to 0') if amount.negative?
+  end
+
+  def unique_product_and_store
+    if new_record? && StockItem.where(product_id: product_id, store_id: store_id).exists?
+      errors.add(:unique_product_and_store, 'This stock_item already be registered.')
+    end
+  end
 end
